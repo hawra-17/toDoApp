49590627482c426b84caa850828f8103a9323678
Hawra Alaradi <123960405+hawra-17@users.noreply.github.com>
2025-06-03 20:07:12 +0300

    setUp

---
+++ app/_layout.jsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+import "../global.css";
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
